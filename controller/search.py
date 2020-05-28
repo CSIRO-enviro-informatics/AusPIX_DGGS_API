3ecc36f305e3fc493ddf7aca58ed77d6383a628e
Wang, Jevy (L&W, Black Mountain) <Jevy.Wang@csiro.au>
2020-05-28 16:15:53 +1000

    geojson validation

--- controller/search.py
+++ controller/search.py
@@ -7,32 +7,80 @@ from shapely.geometry import Polygon, shape
 from auspixdggs.callablemodules.point_DGGSvalue import latlong_to_DGGS
 from auspixdggs.callablemodules.call_DGGS import poly_to_DGGS_tool
 import json
+import geojson
 
 api = Namespace('search', description="Search from DGGS Engine", version="0.1")
+resolutionParser = reqparse.RequestParser()
+resolutionParser.add_argument('resolution', type=int, required=True, choices=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], help='DGGS Resolution 1 to 14')
+resolutionParser.add_argument('geojson', type=dict, location='json')
 
-polygon = api.model('PolygonGeometry', {
-    'type': fields.String(required=True, default="Polygon"),
-    'coordinates': fields.List(
-        fields.List(
-            fields.List(fields.Float, required=True, type="Array"),
-            required=True, type="Array"
-     ))
-})
+def bbox(coord_list):
+     box = []
+     for i in (0,1):
+         res = sorted(coord_list, key=lambda x:x[i])
+         box.append((res[0][i],res[-1][i]))
+     ret = [ box[0][0], box[1][0], box[0][1], box[1][1] ]
+     return ret
 
-linestring = api.model('LineStringGeometry', {
-    'type': fields.String(required=True, default="LineString"),
-    'coordinates': fields.List(
-        fields.List(fields.Float, required=True, type="Array"),
-        required=True,
-        type="Array"
-     )
-})
+def get_cells_from_multi_polygon(geojson_multi_polygon, resolution):
+    cells = []
+    for polygon in geojson_multi_polygon:
+        cells.extend(get_cells_from_polygon(polygon, resolution))
+    return cells
+
+def get_cells_from_polygon(geojson_polygon, resolution):
+    polygon_obj = {"type": "Polygon", "coordinates": geojson_polygon}
+    polygon = shape(polygon_obj)
+    polygon.bbox = bbox(list(coords(polygon_obj)))
+    polygon.points = geojson_polygon[0]
+    cells =  poly_to_DGGS_tool(polygon, '', resolution)
+    if isinstance(cells[0], list):
+        return [cells[0][0]]
+    return cells
+
+@api.route('/find_dggs_by_geojson')
+@api.doc(parser=resolutionParser)
+class FindDGGSByGeojson(Resource):
+    def post(self):
+        args = resolutionParser.parse_args()
+        geojson_obj = geojson.loads(json.dumps(request.json))
+        if not hasattr(geojson_obj, 'is_valid'):
+            return {
+                "error": "geojson is invalid."
+            }
+        if not geojson_obj.is_valid:
+            return {
+                "error": "geojson is invalid.",
+                "detail": geojson_obj.errors()
+            }
+
+        cells = []
+        for feature in geojson_obj["features"]:
+            feature_type = feature["type"]
+            geometry = feature["geometry"]
+            if geometry["type"] == "MultiPolygon":
+                cells.extend(get_cells_from_multi_polygon(geometry["coordinates"], args.resolution))
+            elif geometry["type"] == "Polygon":
+                cells.extend(get_cells_from_polygon(geometry["coordinates"], args.resolution))
+        meta = {
+            "type": geojson_obj["type"],
+            "features_count": len(geojson_obj["features"]),
+            "cells_count": len(cells)
+        }
+        return {
+            "meta": meta,
+            "dggs_cells": cells,
+            "payload": request.json
+        }
+        return {}
+
+
 
 pointerParser = reqparse.RequestParser()
 pointerParser.add_argument('x', type=float, required=True, help='Coordinate X')
 pointerParser.add_argument('y', type=float, required=True, help='Coordinate Y')
 pointerParser.add_argument('epsg', type=int, required=True, help='spsg code, such as 4326')
-pointerParser.add_argument('resolution', type=int, required=True, choices=[1,2,3,4,5,6,7,8,9,10,11,12,13,14], help='DGGS Resolution 1 to 14')
+pointerParser.add_argument('resolution', type=int, required=True, choices=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], help='DGGS Resolution 1 to 14')
 
 @api.route('/find_dggs_for_a_point')
 class FindDGGSForPoint(Resource):
@@ -64,42 +112,43 @@ class FindDGGSForPoint(Resource):
         except exceptions.CRSError:
             return api.abort(500, message='Please input a valid epsg code')
 
-resolutionParser = reqparse.RequestParser()
-resolutionParser.add_argument('resolution', type=int, required=True, choices=[1,2,3,4,5,6,7,8,9,10,11,12,13,14], help='DGGS Resolution 1 to 14')
+# linestring = api.model('LineStringGeometry', {
+#     'type': fields.String(required=True, default="LineString"),
+#     'coordinates': fields.List(
+#         fields.List(fields.Float, required=True, type="Array"),
+#         required=True,
+#         type="Array",
+#         default=[[13.420143127441406, 52.515594085869914],
+#                  [13.421173095703125, 52.50535544522142],
+#                  [13.421173095703125, 52.49532344352079]]
+#      )
+# })
+# @api.route('/find_dggs_for_a_line')
+# @api.doc(parser=resolutionParser)
+# class FindDGGSForALine(Resource):
+#     @api.expect(linestring,  validate=True)
+#     def post(self):
+#         # no find dggs for a line function found at DGGS Engine
+#         pass
 
-@api.route('/find_dggs_for_a_line')
-@api.doc(parser=resolutionParser)
-class FindDGGSForALine(Resource):
-    @api.expect(linestring,  validate=True)
-    def post(self):
-        # no find dggs for a line function found at DGGS Engine
-        pass
-
-def bbox(coord_list):
-     box = []
-     for i in (0,1):
-         res = sorted(coord_list, key=lambda x:x[i])
-         box.append((res[0][i],res[-1][i]))
-     ret = [ box[0][0], box[1][0], box[0][1], box[1][1] ]
-     return ret
-@api.route('/find_dggs_cells_within_polygon')
-@api.doc(parser=resolutionParser)
-class FindDGGSForALine(Resource):
-    @api.expect(polygon,  validate=True)
-    def post(self):
-        args = resolutionParser.parse_args()
-        polygon = shape(request.json)
-        polygon.bbox = bbox(list(coords(request.json)))
-        polygon.points = request.json['coordinates'][0]
-        cells = poly_to_DGGS_tool(polygon, '', args.resolution) 
-        meta = {
-            "count": len(cells),
-            "polygon": request.json
-        }
-        return {
-            "meta": meta,
-            "dggs_cells": cells
-        }
+# @api.route('/find_dggs_cells_within_polygon')
+# @api.doc(parser=resolutionParser)
+# class FindDGGSForALine(Resource):
+#     @api.expect(feature_model,  validate=True)
+#     def post(self):
+#         args = resolutionParser.parse_args()
+#         polygon = shape(request.json)
+#         polygon.bbox = bbox(list(coords(request.json)))
+#         polygon.points = request.json['coordinates'][0]
+#         cells = poly_to_DGGS_tool(polygon, '', args.resolution) 
+#         meta = {
+#             "count": len(cells),
+#             "polygon": request.json
+#         }
+#         return {
+#             "meta": meta,
+#             "dggs_cells": cells
+#         }
 
 
 
--- tests/test_search_namespace.py
+++ tests/test_search_namespace.py
@@ -17,35 +17,45 @@ class TestSequenceFunctions(unittest.TestCase):
         res = self.client.get('/api/search/find_dggs_for_a_point?x=149.124413&y=-35.307934&epsg=4326&resolution=7')
         assert json.loads(res.data)['dggs_cell_id'] == 'R7852372'
 
-    def test_find_dggs_cells_within_polygon(self):
-        geoJson = {
-            "type": "Polygon",
-            "coordinates": [
-            [
-                [
-                149.07223105430603,
-                -35.241045155903464
-                ],
-                [
-                149.0769517421722,
-                -35.241045155903464
-                ],
-                [
-                149.0769517421722,
-                -35.23764520319855
-                ],
-                [
-                149.07223105430603,
-                -35.23764520319855
-                ],
-                [
-                149.07223105430603,
-                -35.241045155903464
-                ]
-            ]
+    # Test Polygon
+    def test_find_dggs_cells_by_geojson(self):
+        geo_json = {
+            "type": "FeatureCollection",
+            "name": "Belconnen",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                [
+                                149.07223105430603,
+                                -35.241045155903464
+                                ],
+                                [
+                                149.0769517421722,
+                                -35.241045155903464
+                                ],
+                                [
+                                149.0769517421722,
+                                -35.23764520319855
+                                ],
+                                [
+                                149.07223105430603,
+                                -35.23764520319855
+                                ],
+                                [
+                                149.07223105430603,
+                                -35.241045155903464
+                                ]
+                            ]
+                     ]
+                    }
+                }
             ]
         }
-        res = self.client.post('api/search/find_dggs_cells_within_polygon?resolution=10', json=geoJson)
+        res = self.client.post('api/search/find_dggs_by_geojson?resolution=10', json=geo_json)
         result = [
             "R7852344443",
             "R7852344444",
@@ -55,7 +65,19 @@ class TestSequenceFunctions(unittest.TestCase):
             "R7852344448"
         ]
         self.assertListEqual(json.loads(res.data)['dggs_cells'], result)
-
+    # Test multi polygon
+    def get_find_dggs_cells_by_geojson_multi_plygon(self):
+        with open("ACT_SA1_Black_Mountain.geojson", "r") as file:
+            geo_json = json.loads(file.read)
+            res = self.client.post('api/search/find_dggs_by_geojson?resolution=8', json=geo_json)
+            result = [
+                "R78523472",
+                "R78523480",
+                "R78523475",
+                "R78523483",
+                "R78523477"
+            ]
+            self.assertListEqual(json.loads(res.data)['dggs_cells'], result)
 if __name__ == '__main__':
     unittest.main()
 
